5d0bbe5a3dd0f4960bd2ed75656763dde222b257
Jose Quintero <jolquinteroc@correo.udistrital.edu.co>
2022-09-07 01:14:25 -0500

    ajustes gitignore

--- src/logica/Led.java
+++ src/logica/Led.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
 
 import javax.swing.JLabel;
 
-/**
- *
- * @author Estudiantes
- */
 public class Led {
     private JLabel lbl;
     private int estado;
